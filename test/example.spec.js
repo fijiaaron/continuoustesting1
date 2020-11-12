ba33f1ccf8d3169b8cfa68057e9ed0126639c6f2
Aaron Evans <aarone@one-shore.com>
2020-11-12 04:16:19 -0700

    add test

--- test/example.spec.js
+++ test/example.spec.js
@@ -3,6 +3,10 @@ const { assert, expect } = require("chai")
 describe("This is an example test", function() {
     
     it("It should test something", ()=> {
-        expect(1+2).to.equal(3);
+        expect(1 + 2).to.equal(3);
+    });
+
+    it("It should test something else", ()=> {
+        expect("hello" + "world").to.equal("helloworld");
     });
 });
