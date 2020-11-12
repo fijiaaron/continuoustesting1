21e586b6cbf693a579f135dda522a8b01fad7d18
Aaron Evans <aarone@one-shore.com>
2020-11-12 00:05:30 -0700

    add tests and manual workflow

---
+++ test/example.spec.js
@@ -0,0 +1,8 @@
+const { assert, expect } = require("chai")
+
+describe("This is an example test", function() {
+    
+    it("It should test something", ()=> {
+        expect(1+2).to.equal(3);
+    });
+});
